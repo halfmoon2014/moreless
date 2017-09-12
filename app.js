0e89150f038399225661773b2e8b7e1408687a7d
win10min <mrzhangmh@gmail.com>
2017-09-12 22:24:03 +0800

    test

--- app.js
+++ app.js
@@ -22,8 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', index);
 app.use('/users', users);
+
+var birds = require('./routes/birds');
+app.use('/birds', birds);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,7 +41,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("hello world2fffff");
-  //res.render('index', { title: 'Express' });
+  //res.send("hello world index");
+  res.render('index', { title: 'Express',mdata:'mydata' });
 });
 
 module.exports = router;
